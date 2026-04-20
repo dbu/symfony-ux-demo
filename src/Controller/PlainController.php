080929067196e0b6ae566008a4dd4cc42d9a7dca
David Buchmann <david.buchmann@liip.ch>
2026-04-20 13:29:16 +0200

    add slow plain symfony page and turbo drive page to compare

---
+++ src/Controller/PlainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PlainController extends AbstractController
+{
+    #[Route('/plain', name: 'app_plain')]
+    public function index(): Response
+    {
+        return $this->render('plain/index.html.twig');
+    }
+
+    #[Route('/plain-slow', name: 'app_plain_slow')]
+    public function slow(SessionInterface $session): Response
+    {
+        $count = $session->get('plain-call-count', 0) + 1;
+        $session->set('plain-call-count', $count);
+
+        sleep(3);
+
+        return $this->render('plain/slow.html.twig', [
+            'callCount' => $count,
+        ]);
+    }
+}
---
+++ src/Controller/TurboDriveController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class TurboDriveController extends AbstractController
+{
+    #[Route('/drive', name: 'app_drive')]
+    public function index(): Response
+    {
+        return $this->render('turbo_drive/index.html.twig', [
+            'controller_name' => 'TurboDriveController',
+        ]);
+    }
+
+    #[Route('/drive-slow', name: 'app_drive_slow')]
+    public function slow(SessionInterface $session): Response
+    {
+        $count = $session->get('turbo-call-count', 0) + 1;
+        $session->set('turbo-call-count', $count);
+
+        sleep(3);
+
+        return $this->render('turbo_drive/slow.html.twig', [
+            'callCount' => $count,
+        ]);
+    }
+}
